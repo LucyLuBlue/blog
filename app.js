11e331b40fb1f735d1225ec91fa4f357558e265f
LucyLuBlue <lucylu858@gmail.com>
2019-12-01 17:59:35 +0000

    debug2

--- app.js
+++ app.js
@@ -68,12 +68,12 @@ router.post('/post/delete', function(req, res) {
   // Function to read in a JSON file, add to it & convert to XML
   function deleteJSON(obj) {
     // Function to read in XML file, convert it to JSON, delete the required object and write back to XML file
-    xmlFileToJs('PaddysCafe.xml', function(err, result) {
+    xmlFileToJs('project.xml', function(err, result) {
       if (err) throw (err);
       //This is where we delete the object based on the position of the section and position of the entree, as being passed on from index.html
       delete result.cafemenu.section[obj.section].entree[obj.entree];
       //This is where we convert from JSON and write back our XML file
-      jsToXmlFile('PaddysCafe.xml', result, function(err) {
+      jsToXmlFile('project.xml', result, function(err) {
         if (err) console.log(err);
       })
     })
